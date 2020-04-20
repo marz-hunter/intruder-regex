0339d95c4b355f618b31d186b334f8f9335367ec
Marzuki <58464282+marz-hunter@users.noreply.github.com>
2020-04-20 07:37:57 +0700

    Create brute.sh

---
+++ brute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat 1.txt | while read url; do ffuf -mc 200 -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$url" -w wordlist/dicc.txt -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -o result_dir.tmp;done
+cat result_dir.tmp | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > result_dir.txt
+printf "\nDone. Result is stored in result_dir.txt\n"
